0313567b1f82d7a40ed0aff6dc26caf1544958ac
blopez <blopez@bdgsa.net>
2019-04-23 19:11:21 -0600

    Finished loading new excels

--- app.js
+++ app.js
@@ -3,11 +3,14 @@
 const fs = require('fs');
 const folder = './jsonFiles/';
 var _ = require('lodash');
+var excel = require('exceljs');
+
 
 fs.readdir(folder, (err, files) => {
   files.forEach(file => {
     let parseFile = parsedFile(folder + file)
     let toExcel = orderJson(parseFile)
+    let generate = generateNewExcel(toExcel)
   });
 });
 
@@ -39,5 +42,26 @@ const orderJson = (arg) => {
     let statusGroup = _.groupBy(value, "Status");
     result.push({numeroTienda: key, status: statusGroup, name: nameStore, country: country, format: format})
   },[]).value();
-  console.log(groupStore)
+  return groupStore;
+}
+
+const generateNewExcel = (arg) => {
+  let createWB = _.map(arg, function(data,llave){
+    var workbook = new excel.Workbook();
+    var storeSheet = workbook.addWorksheet('Tienda');
+    storeSheet.columns = [{header: 'Pais', key: 'Pais'}, {header: 'Tienda', key: 'Tienda'},{header: 'Nombre', key: 'Nombre'}, {header: 'Formato', key: 'Formato'}]
+    storeSheet.addRow({Pais: data.country, Tienda: data.numeroTienda, Nombre: data.name, Formato: data.format});
+    let statusArray = _.map(data.status, function(value,key){
+        let deptoSheet = workbook.addWorksheet(key);
+        deptoSheet.columns = [{header: 'Clasificacion', key: 'Clasificacion'}, {header: 'Depto', key: 'Depto'}, {header: 'Categoria', key: 'Categoria'}]
+        let aux = _.map(value, function(val,k){
+            deptoSheet.addRow({Clasificacion: val.Content.Clasificacion, Depto: val.Content.Depto, Categoria: val.Content.Categoria})
+        })
+    })
+    console.log(data.numeroTienda)
+    workbook.xlsx.writeFile("./"+data.name+".xlsx").then(function() {
+      console.log("xls file is written.");
+    });
+  });
+
 }
